3922e81c9e820a673b934d8905930ee18b92df97
Mahamd Abomesalm <109667633+mesalm@users.noreply.github.com>
2024-08-12 14:04:48 +0300

    أضفت مكتبة هيلمت وقمت بإعداد محدد المعدل (Rate Limiter)

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const app=express();
+const rateLimit = require('express-rate-limit')
 app.use(express.json())
 const logger = require('morgan');
 app.use(logger('dev'))
+const helmet = require('helmet');
+app.use(helmet());
 const dotenv = require('dotenv').config();
 const db_connection = require('./config/db_coniction')
 const swaggerSetup = require('./swaggerUi ');
 swaggerSetup(app);
 db_connection()
+
+const limiter = rateLimit({
+    windowMs :15*60*1000,
+    max : 100,
+    message :'Too many requests from this IP , please try again after 15 minutes'
+})
+app.use(limiter);
 
 
 app.use("/api/users", require('./routs/userRouts')); 
